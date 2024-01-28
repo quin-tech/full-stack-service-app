fbdae10c6f87536d96fc2d364f6cfb9fbc4657b9
JW <daxon2012@gmail.com>
2024-01-28 05:45:54 -0800

    Working PWA

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,49 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from "vite-plugin-pwa";
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      manifest: {
+        name: "Skill Magnet",
+        short_name: "Skill.M",
+        description: "Shop For Skills Even offline",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ],
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+    }),
+  ],
   server: {
     port: 3000,
     open: true,
